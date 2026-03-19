4a49028da2997c5000c480c7850c5c3da1e9cc8d
Alexandra <alexandracasanassalazar@gmail.com>
2026-03-19 15:20:19 -0600

    actualización con libro,imagenes y musica de fonde con botones

--- interaccion.js
+++ interaccion.js
@@ -1,30 +1,37 @@
+// --- Navegación del libro ---
 let currentPage = 0;
 const pages = document.querySelectorAll('.page');
 
 // Mostrar solo la primera página al inicio
 pages.forEach((p, i) => p.style.display = i === 0 ? 'flex' : 'none');
 
+// Botón siguiente página
 document.getElementById('next').addEventListener('click', () => {
-  console.log("Botón NEXT presionado");
   pages[currentPage].style.display = 'none';
   currentPage = (currentPage + 1) % pages.length;
   pages[currentPage].style.display = 'flex';
 });
 
+// Botón página anterior
 document.getElementById('prev').addEventListener('click', () => {
-  console.log("Botón PREV presionado");
   pages[currentPage].style.display = 'none';
   currentPage = (currentPage - 1 + pages.length) % pages.length;
   pages[currentPage].style.display = 'flex';
 });
 
-
+// --- Música y sabicat ---
 const sabicat = document.querySelector('.sabicat');
 const audio = document.getElementById('bg-music');
 
+// Lista de canciones
 const playlist = [
   "canciones/Amapolas.mp3",
-  "canciones/Sweet Child O' Mine.mp3"
+  "canciones/meteora.mp3",
+  "canciones/Algo quiero.mp3",
+  "canciones/El Fin del Mundo.mp3",
+  "canciones/The First Time.mp3",
+  "canciones/Zombie Lady.mp3",
+  "canciones/amarrame.mp3"
 ];
 
 let currentSong = 0;
